4c66de6acb7815435d2ea4488f02224665210a3c
oilujc <jcmartinezvalero@gmail.com>
2018-01-03 10:49:44 -0400

    update files

---
+++ public/inde2x.php
@@ -0,0 +1,8 @@
+<?php 
+	//define("SERVER_DIR", "/home/rebar/rebardomains");
+
+	require_once "../core/app.php";
+
+	$app->run();
+
+ ?>
--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
-<?php 
-	//define("SERVER_DIR", "/home/rebar/rebardomains");
-
-	require_once __DIR__."/../core/app.php";
-
-	$app->run();
-
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Example doc</title>
+</head>
+<body>
+	<h1>php slim app</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione doloremque laboriosam repellendus earum aliquid ab fugit nam, consequatur vel, non consectetur. Eaque culpa ipsa perferendis sunt assumenda! Porro, incidunt!</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione doloremque laboriosam repellendus earum aliquid ab fugit nam, consequatur vel, non consectetur. Eaque culpa ipsa perferendis sunt assumenda! Porro, incidunt!</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione doloremque laboriosam repellendus earum aliquid ab fugit nam, consequatur vel, non consectetur. Eaque culpa ipsa perferendis sunt assumenda! Porro, incidunt!</p>
+</body>
+</html>
